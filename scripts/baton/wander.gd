44bf9749a82685c9c2a29423027f68d811da0bf3
yashGhela <ghelayash@gmail.com>
2026-08-22 21:56:19 +0200

    BOY FUCK TS

--- scripts/baton/wander.gd
+++ scripts/baton/wander.gd
@@ -1,88 +1,30 @@
 extends State
 
-@export var enemy: CharacterBody3D
-@export var move_speed := 4.0
-
-var player: CharacterBody3D
-var navagent: NavigationAgent3D
-
-@onready var timer: Timer = $"../../Timer"
+@export var enemy:CharacterBody3D
 
 
+var move_direction:Vector3
+var wander_time:float
+var player:CharacterBody3D
+@export var move_speed:=0.2
+@onready var ap: AnimationPlayer = $"../../pincer/AnimationPlayer"
+
+
+func randomize_wander():
+	move_direction = Vector3(randf_range(-0.5, 0.5), 0, randf_range(-1, 1)).normalized()
+	wander_time = randf_range(1,3)
+	
+	
 func enter():
-	if not enemy:
-		return
-
-	navagent = enemy.get_node("NavigationAgent3D")
-	player = get_tree().get_first_node_in_group("Player")
-
-	if not timer.timeout.is_connected(_on_timer_timeout):
-		timer.timeout.connect(_on_timer_timeout)
-
-	if not navagent.target_reached.is_connected(_on_target_reached):
-		navagent.target_reached.connect(_on_target_reached)
-
-	await get_tree().physics_frame
-	await get_tree().physics_frame
-
-	print("NAV MAP: ", navagent.get_navigation_map())
-
-	pick_new_target()
+	print("Entered Wander")
+	randomize_wander()
+	player= get_tree().get_first_node_in_group("Player")
 
 
-func physics_update(_delta: float):
-	if not enemy or not navagent:
-		return
-
-	if navagent.is_navigation_finished():
-		enemy.mv = Vector3.ZERO
-		return
-
-	var npp: Vector3 = navagent.get_next_path_position()
-
-	var dir := enemy.global_position.direction_to(npp)
-
-	# Only move horizontally
-	dir.y = 0
-	dir = dir.normalized()
-
-	enemy.mv = dir * move_speed
-
-	var playerdist = enemy.global_position.distance_to(
-		player.global_position
-	)
-
-	if playerdist < 6.0:
-		Transitioned.emit(self, "Chase")
-
-
-func pick_new_target():
-	if not navagent:
-		return
-
-	var map = navagent.get_navigation_map()
-
-	if map == RID():
-		print("NO NAVIGATION MAP!")
-		return
-
-	
-	var point = NavigationServer3D.map_get_random_point(map,1,false)
-	
-	var map_bounds = 50.0  # Example: 50 units from center
-	point.x = clamp(point.x, -map_bounds, map_bounds)
-	point.z = clamp(point.z, -map_bounds, map_bounds)
-	
-	print(point)
-	# Optionally, project the point back onto the navigation mesh
-	point = NavigationServer3D.map_get_closest_point(map, point)
-	
-	navagent.target_position = point
-
-
-func _on_timer_timeout():
-	pick_new_target()
-
-
-func _on_target_reached():
-	pick_new_target()
+func physics_update(_delta:float):
+	if enemy:
+		enemy.mv = move_direction* move_speed
+	ap.play("Cube|move")
+	var playerdist = enemy.global_position.distance_to(player.global_position)
+	if playerdist<3.0:
+		Transitioned.emit(self,"Pince")
